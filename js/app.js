0134c708a537883d913033a408c1815004a170cd
fsoc <fsoc@altai.se>
2013-08-13 12:37:34 +0200

    Added a JQuery and mockup of a site.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////
+
+$(document).ready(function() {
+	var root = new RootPanel("bootstrap");
+	var wrapper = new FlowPanel();
+	wrapper.setId("wrapper");
+
+  root.add(wrapper);
+});
+
+
