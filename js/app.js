a707bd6ebbf8ca42ae5e75b15961a520c5ec74ce
fsoc <fsoc@altai.se>
2013-08-13 13:32:07 +0200

    Created todos header.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
+var headerView = FlowPanel.extend({
+  init: function() {
+    this._super();
+    this.setId("header");
+    this.render();
+  },
+  render: function() {
+    var h1 = new Header1(["todos"]);
+    this.add(h1);
+  }
+});
+
 ////////////////////////////////////////////////////
 
 $(document).ready(function() {
-	var root = new RootPanel("bootstrap");
+	var root = new RootPanel("boostrap");
 	var wrapper = new FlowPanel();
-	wrapper.setId("wrapper");
+	wrapper.setId("todoapp");
+
+  var header = new headerView();
+
+  wrapper.add(header);
 
   root.add(wrapper);
 });
